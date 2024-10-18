39c8370fb71d52de7685a085cba51aa0da6b5ca0
andreilance06 <andreilanceasuncion@gmail.com>
2024-10-18 23:53:36 +0800

    fix: videos inside directories are now playable

--- routes.go
+++ routes.go
@@ -12,6 +12,6 @@ func RegisterRoutes(mux *http.ServeMux, c *torrent.Client, config *ClientConfig,
 	mux.Handle("POST /torrents", HandlePostTorrents(c, config))
 	mux.Handle("GET /torrents/{infohash}", HandleGetInfoHash(c, config))
 	mux.Handle("DELETE /torrents/{infohash}", HandleDeleteInfoHash(c, config))
-	mux.Handle("GET /torrents/{infohash}/{query}", HandleGetInfoHashFile(c, config))
+	mux.Handle("GET /torrents/{infohash}/{query...}", HandleGetInfoHashFile(c, config))
 	mux.Handle("GET /exit", HandleExit(cancel))
 }
